1ea91eadd5d269b5aa268fe1bd5ea1ad55f50d04
Piotr Szyma <thompson2908@gmail.com>
2019-10-21 11:05:45 +0200

    protocol examples folder

--- server/protocols_examples/msis.js
+++ server/protocols_examples/msis.js
@@ -1,9 +1,9 @@
 const mcl = require('mcl-wasm');
 
-
 async function protocolExecutionExample() {
   await mcl.init(mcl.BLS12_381);
 
+  // Client generates Q, a, x, calculates A, X and sends them to server.
   const Q = mcl.hashAndMapToG2('genQ');
   console.log(`Q: ${Q.getStr().slice(2)}`);
   const a = new mcl.Fr();
@@ -14,13 +14,15 @@ async function protocolExecutionExample() {
   x.setByCSPRNG();
   const X = mcl.mul(Q, x);
 
+  // Server receives A, X and calculates c and sends c to client.
   const c = new mcl.Fr();
   c.setByCSPRNG();
 
+  // Client receives c, calculates s, U and sends to server.
   const s = mcl.add(x, mcl.mul(a, c))
-
   const U = mcl.hashAndMapToG1(X.serializeToHexStr() + c.serializeToHexStr());
 
+  // Server receives s, U, calculates pairings and compares e1 & e2.
   const S = mcl.mul(U, s);
   const AC = mcl.mul(A, c)
   const XAC = mcl.add(X, AC)

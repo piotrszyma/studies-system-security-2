d0e2107922b5e507764fb836fe4c1af131d5bfb2
Piotr Szyma <thompson2908@gmail.com>
2019-12-05 13:52:46 +0100

    chacha & salsa fixes

--- server/routes/encryptions/chacha.js
+++ server/routes/encryptions/chacha.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const crypto = require('crypto');
+const chacha = require('chacha');
 const fs = require('fs');
 const { asyncMiddleware } = require('../../utils');
 const axios = require('axios');
@@ -10,8 +11,57 @@ const config = require('../../config');
 const ADDRESS = config.serverConfig.address;
 const PORT = config.serverConfig.httpsPort;
 
-router.all('*', asyncMiddleware(async (req, res, next) => {
+
+router.get('*', asyncMiddleware(async (req, res, next) => {
+  // const { ciphertext: ciphertextb64, nonce: nonceb64 } = req.body;
+
+  // const ciphertext = Buffer.from(ciphertextb64, 'base64');
+  // const requestNonce = Buffer.from(nonceb64, 'base64');
+
+  // const decipherFactory = crypto.createDecipheriv(
+  //   'chacha20-poly1305', CHACHA_KEY, requestNonce);
+
+  // const decrypted = decipherFactory.update(ciphertext);
+
+  // const data = JSON.parse(decrypted.toString());
+
+  const path = req.path;
+
+  let response;
+  try {
+    response = await axios.get(`https://${ADDRESS}:${PORT}/protocols${path}`);
+  } catch (error) {
+    throw error.response.data;
+  }
+
+  const msg = JSON.stringify(response.data);
+  const responseNonce = crypto.randomBytes(12);
+  const cipherFactory = chacha.createCipher(CHACHA_KEY, responseNonce);
+  const responseCiphertext = cipherFactory.update(msg, 'utf8');
+  await cipherFactory.final();
+  const tag = cipherFactory.getAuthTag();
+  res.send({
+    ciphertext: responseCiphertext.toString('base64'),
+    nonce: responseNonce.toString('base64'),
+    tag: tag.toString('base64'),
+  });
+
+  // const msg = JSON.stringify(response.data);
+  // const responseNonce = crypto.randomBytes(12);
+  // const cipherFactory = crypto.createCipheriv(
+  //   'chacha20-poly1305', CHACHA_KEY, responseNonce);
+  // const responseCiphertext = cipherFactory.update(msg);
+
+  // res.send({
+  //   ciphertext: responseCiphertext.toString('base64'),
+  //   nonce: responseNonce.toString('base64'),
+  // });
+}));
+
+
+router.post('*', asyncMiddleware(async (req, res, next) => {
   const { ciphertext: ciphertextb64, nonce: nonceb64 } = req.body;
+  // TODO: Analyze if tag is needed here (maybe)
 
   const ciphertext = Buffer.from(ciphertextb64, 'base64');
   const requestNonce = Buffer.from(nonceb64, 'base64');
@@ -34,13 +84,14 @@ router.all('*', asyncMiddleware(async (req, res, next) => {
 
   const msg = JSON.stringify(response.data);
   const responseNonce = crypto.randomBytes(12);
-  const cipherFactory = crypto.createCipheriv(
-    'chacha20-poly1305', CHACHA_KEY, responseNonce);
-  const responseCiphertext = cipherFactory.update(msg);
-
+  const cipherFactory = chacha.createCipher(CHACHA_KEY, responseNonce);
+  const responseCiphertext = cipherFactory.update(msg, 'utf8');
+  await cipherFactory.final();
+  const tag = cipherFactory.getAuthTag();
   res.send({
     ciphertext: responseCiphertext.toString('base64'),
     nonce: responseNonce.toString('base64'),
+    tag: tag.toString('base64'),
   });
 }));
 
